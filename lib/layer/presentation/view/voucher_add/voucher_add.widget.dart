5c1173091f138f3d56921a9ae87ebd45f5c6b51c
Seheon Yu <developer@seheon.kr>
2023-08-17 15:51:22 +0900

    feat: Add voucher register button

---
+++ lib/layer/presentation/view/voucher_add/voucher_add.widget.dart
@@ -0,0 +1,11 @@
+// 🐦 Flutter imports:
+import 'package:flutter/widgets.dart';
+
+class VoucherRegister extends StatelessWidget {
+  const VoucherRegister({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
