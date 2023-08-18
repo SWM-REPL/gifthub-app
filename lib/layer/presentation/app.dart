74af944e89e1fa3e3b2cf5bf62f10cc6438d267b
Seheon Yu <developer@seheon.kr>
2023-08-19 08:49:01 +0900

    feature: Receive sharing data from another app
    (add) Sharing extension - iOS
    (add) Receive sharing intent - Android

--- lib/layer/presentation/app.dart
+++ lib/layer/presentation/app.dart
@@ -1,17 +1,69 @@
+// 🎯 Dart imports:
+import 'dart:async';
+
 // 🐦 Flutter imports:
 import 'package:flutter/material.dart';
 
 // 📦 Package imports:
 import 'package:google_fonts/google_fonts.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
 // 🌎 Project imports:
-import 'package:gifthub/layer/presentation/view/voucher_list/voucher_list.widget.dart';
 import 'package:gifthub/theme/appbar.theme.dart';
 import 'package:gifthub/theme/button.theme.dart';
 import 'package:gifthub/theme/color.theme.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late StreamSubscription _mediaStreamSubscription;
+  late StreamSubscription _textStreamSubscription;
+
+  List<SharedMediaFile> _sharedFiles = [];
+  String _sharedText = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    _mediaStreamSubscription = ReceiveSharingIntent.getMediaStream()
+        .listen((List<SharedMediaFile> value) {
+      setState(() {
+        _sharedFiles = value;
+      });
+    });
+
+    ReceiveSharingIntent.getInitialMedia().then((List<SharedMediaFile> value) {
+      setState(() {
+        _sharedFiles = value;
+      });
+    });
+
+    _textStreamSubscription =
+        ReceiveSharingIntent.getTextStream().listen((String value) {
+      setState(() {
+        _sharedText = value;
+      });
+    });
+
+    ReceiveSharingIntent.getInitialText().then((String? value) {
+      setState(() {
+        _sharedText = value ?? '';
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _mediaStreamSubscription.cancel();
+    _textStreamSubscription.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +77,25 @@ class App extends StatelessWidget {
         outlinedButtonTheme: const GifthubOutlinedButtonThemeData(),
         elevatedButtonTheme: const GifthubElevatedButtonThemeData(),
       ),
-      home: const VoucherList(),
+      // home: const VoucherList(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              const Text('Shared files:',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(_sharedFiles.map((f) => f.path).join(',')),
+              const SizedBox(height: 100),
+              const Text('Shared urls/text:',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(_sharedText)
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
