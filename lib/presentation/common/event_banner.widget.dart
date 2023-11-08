eb4b401f0c9425672f7bf687244e91f6e8518f90
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2023-11-08 14:38:27 +0900

    Refactor event banner widget to use remote config

--- lib/presentation/common/event_banner.widget.dart
+++ lib/presentation/common/event_banner.widget.dart
@@ -1,60 +1,48 @@
-// 🎯 Dart imports:
-import 'dart:async';
-
 // 🐦 Flutter imports:
 import 'package:flutter/material.dart';
 
 // 📦 Package imports:
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 // 🌎 Project imports:
 import 'package:gifthub/constants.dart';
-import 'package:gifthub/domain/entities/remote_config.entity.dart';
+import 'package:gifthub/presentation/common/loading.widget.dart';
 import 'package:gifthub/presentation/common/voucher_card.widget.dart';
 import 'package:gifthub/presentation/providers/config.provider.dart';
 
-class EventBanner extends ConsumerStatefulWidget {
+class EventBanner extends ConsumerWidget {
   const EventBanner({super.key});
 
   @override
-  ConsumerState<EventBanner> createState() => _EventBannerState();
-}
-
-class _EventBannerState extends ConsumerState<EventBanner> {
-  final timer = Timer.periodic(const Duration(seconds: 1), (timer) {});
-
-  List<Event> events = [];
-
-  @override
-  void initState() {
-    super.initState();
-    Future.microtask(() async {
-      final config = await ref.watch(remoteConfigProvider.future);
-      setState(() {
-        for (var e in config.events) {
-          events.add(e);
-        }
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => handleTapInside(),
-      child: Card(
-        margin: const EdgeInsets.all(GiftHubConstants.padding),
-        child: SizedBox(
-          height: VoucherCard.height + VoucherCard.padding * 2,
-          width: double.infinity,
-          child: Image.asset('assets/event-banner.png'),
-        ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final remoteConfig = ref.watch(remoteConfigProvider);
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: VoucherCard.height / 0.8,
+        viewportFraction: 0.8,
+        autoPlay: true,
+      ),
+      items: remoteConfig.when(
+        data: (config) => config.events
+            .map((e) => Builder(
+                  builder: (context) => GestureDetector(
+                    onTap: () => launchUrl(Uri.parse(e.clickUrl)),
+                    child: Card(
+                      margin: const EdgeInsets.all(GiftHubConstants.padding),
+                      child: SizedBox(
+                        height: VoucherCard.height + VoucherCard.padding * 2,
+                        width: double.infinity,
+                        child: Image.network(e.bannerImageUrl),
+                      ),
+                    ),
+                  ),
+                ))
+            .toList(),
+        loading: () => [const Loading()],
+        error: (error, stackTrace) => throw error,
       ),
     );
-  }
-
-  void handleTapInside() {
-    launchUrl(Uri.parse('https://forms.gle/AWAKLJ6PJHXWs3G99'));
   }
 }
