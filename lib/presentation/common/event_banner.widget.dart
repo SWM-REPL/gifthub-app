ade77dc37b8192c11aec7da61e441c3cd33f72c9
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2023-11-08 14:03:49 +0900

    Add event banner widget with dynamic events

--- lib/presentation/common/event_banner.widget.dart
+++ lib/presentation/common/event_banner.widget.dart
@@ -1,19 +1,55 @@
+// 🎯 Dart imports:
+import 'dart:async';
+
 // 🐦 Flutter imports:
 import 'package:flutter/material.dart';
 
 // 📦 Package imports:
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class EventBanner extends StatelessWidget {
+// 🌎 Project imports:
+import 'package:gifthub/constants.dart';
+import 'package:gifthub/domain/entities/remote_config.entity.dart';
+import 'package:gifthub/presentation/common/voucher_card.widget.dart';
+import 'package:gifthub/presentation/providers/config.provider.dart';
+
+class EventBanner extends ConsumerStatefulWidget {
   const EventBanner({super.key});
+
+  @override
+  ConsumerState<EventBanner> createState() => _EventBannerState();
+}
+
+class _EventBannerState extends ConsumerState<EventBanner> {
+  final timer = Timer.periodic(const Duration(seconds: 1), (timer) {});
+
+  List<Event> events = [];
+
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() async {
+      final config = await ref.watch(remoteConfigProvider.future);
+      setState(() {
+        for (var e in config.events) {
+          events.add(e);
+        }
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => handleTapInside(),
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Image.asset('assets/event-banner.png'),
+      child: Card(
+        margin: const EdgeInsets.all(GiftHubConstants.padding),
+        child: SizedBox(
+          height: VoucherCard.height + VoucherCard.padding * 2,
+          width: double.infinity,
+          child: Image.asset('assets/event-banner.png'),
+        ),
       ),
     );
   }
