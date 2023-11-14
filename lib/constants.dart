9c785b5ac84ee71ef29a4ba365a397fc4c7f79d7
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2023-11-14 21:38:55 +0900

    Update API host URL in constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ class GiftHubConstants {
   static const contactUsUri = 'https://gifthub.kr/contact-us';
   static const remoteConfig = 'https://cdn.gifthub.kr/remote-config.json';
   static const host = 'https://gifthub.kr';
-  static const apiHost = 'https://api.dev.gifthub.kr';
+  static const apiHost = 'https://api.gifthub.kr';
   static const appVersion = '0.4.1';
 
   static const padding = 10.0;
